c65a3de8a86dd0c3fee8752b84d3aea5a33b4ddc
Denys Murakhovskyi <dmurakhowsky@gmail.com>
2021-02-10 23:46:43 +0200

    Wrong answers algorithm update

--- quiz_2.py
+++ quiz_2.py
@@ -31,8 +31,49 @@ def create_quiz_from_file(filename):
                     question = Question(text, data)
                 quiz.add_question(question)
             elif command == 'w':
-                question.add_wrong(data)
+                question.add_wrong(Wrong_answer(data))
     return quiz
+
+
+class Wrong_answer:
+    """
+    Class for wrong answers
+    """
+
+    def __init__(self, text):
+        """
+        :param text: text of a wrong answer read from the file
+        """
+        self.text = text
+        self.chosen = 0
+        self.displayed = 0
+
+    def get_text(self):
+        """
+        :return: text of the question
+        """
+        return self.text
+
+    def inc_chosen(self):
+        """
+        Increase counter of answer choices
+        :return:
+        """
+        self.chosen += 1
+
+    def inc_displayed(self):
+        """
+        Increase counter of answer display times
+        :return:
+        """
+        self.displayed += 1
+
+    def score(self):
+        """
+        Calculates score due to the instructions
+        :return: calculated score
+        """
+        return (2 * self.chosen + 1) / (self.displayed + 1)
 
 
 class Question:
@@ -44,7 +85,7 @@ class Question:
         """
         self.question = question  # variable for question
         self.answer = answer  # variable for right answer
-        self.wrong_answers = {}  # dictionary with wrong answers and their frequencies
+        self.wrong_answers = []  # list of wrong answers
 
     def add_wrong(self, wrong_answer):
         """
@@ -52,7 +93,7 @@ class Question:
         :param wrong_answer: wrong answer to add
         :return: None
         """
-        self.wrong_answers.update({wrong_answer: 0})
+        self.wrong_answers.append(wrong_answer)
 
     def ask(self, alternatives=None):
         """
@@ -62,15 +103,21 @@ class Question:
         :return: Tuple(user's answer, right answer)
         """
         # wrong answers choice
-        wrong_answers = []
         if alternatives and (len(self.wrong_answers) >= alternatives - 1):
             wrong_answers = self.get_wrong_answers(alternatives - 1)
         else:
-            wrong_answers = list(self.wrong_answers.keys())  # number of alternatives is not defined
+            wrong_answers = [item.get_text for item in self.wrong_answers]  # number of alternatives is not defined
+                                                                            # or too few wrong answers
 
         answers = [self.answer] + wrong_answers  # full set of answers
         shuffle(answers)  # shuffles the list of answers
         right_answer = answers.index(self.answer) + 1  # defines the number of right answer
+
+        # +1 for displayed answers
+        wa_dict = {item.get_text(): item for item in self.wrong_answers}
+        for item in wrong_answers:
+            wa_dict[item].inc_displayed()
+
         print(self.question)  # prints question in the console
         for answer in enumerate(answers):  # prints answers one after another
             print('{:d}. {:s}'.format(answer[0] + 1, answer[1]))
@@ -78,7 +125,11 @@ class Question:
             users_answer = int(input())
         except ValueError:  # handles value error
             return 0, right_answer
-        return users_answer, right_answer, answers
+
+        # +1 for chosen answer
+        wa_dict[answers[users_answer - 1]].inc_chosen()
+
+        return users_answer, right_answer
 
     def get_wrong_answers(self, n):
         """
@@ -87,22 +138,16 @@ class Question:
         :return: a list of wrong answers
         """
         answers = []
-        freqs = sorted(set(self.wrong_answers.values()), reverse=True)
+        wa_dict = {item: item.score() for item in self.wrong_answers}
+
+        freqs = sorted(set(wa_dict.values()), reverse=True)
         for f in freqs:
-            answers_to_add = [item for item in self.wrong_answers.keys() if self.wrong_answers[item] == f]
+            answers_to_add = [item.get_text() for item in wa_dict.keys() if wa_dict[item] == f]
             shuffle(answers_to_add)
             answers.extend(answers_to_add)
             if len(answers) >= n:
                 break
         return answers[:n]
-
-    def add_freq(self, answer):
-        """
-        +1 to frequency of selected wrong answer
-        :param answer: wrong answer to add frequency
-        :return: None
-        """
-        self.wrong_answers[answer] += 1
 
 
 class IntQuestion(Question):
@@ -147,7 +192,6 @@ class Quiz:
                 counters[0] += 1
             else:
                 print('Sorry, no. Correct answer: {:d}\n'.format(answers[1]))  # if the answer is incorrect
-                question.add_freq(answers[2][answers[0] - 1])
             counters[1] += 1
         print('\n You answered {:d} of {:d}'.format(counters[0], counters[1]))
 
